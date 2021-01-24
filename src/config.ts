c0217f1111ad5927fd3a60a148c54f36132fd29f
Elias Jackson <ejthedj0@gmail.com>
2021-01-23 22:48:28 -0800

    Commands and listeners

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,7 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 export default {
+  prefix: process.env.BOT_PREFIX ?? '',
   ownerID: process.env.BOT_OWNER_ID ?? '',
   token: process.env.DISCORD_API_TOKEN ?? '',
 };
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,17 @@
-import { AkairoClient } from 'discord-akairo';
+import {
+  AkairoClient,
+  CommandHandler,
+  InhibitorHandler,
+  ListenerHandler,
+} from 'discord-akairo';
 import config from './config';
+import * as path from 'path';
 
 class MyClient extends AkairoClient {
+  commandHandler: CommandHandler;
+  inhibitorHandler: InhibitorHandler;
+  listenerHandler: ListenerHandler;
+
   constructor() {
     super(
       {
@@ -11,6 +21,24 @@ class MyClient extends AkairoClient {
         disableMentions: 'everyone',
       }
     );
+
+    this.commandHandler = new CommandHandler(this, {
+      directory: path.join(__dirname, 'commands'),
+      prefix: config.prefix,
+    });
+    this.commandHandler.loadAll();
+
+    this.inhibitorHandler = new InhibitorHandler(this, {
+      directory: path.join(__dirname, 'inhibitors'),
+    });
+    this.commandHandler.useInhibitorHandler(this.inhibitorHandler);
+    this.inhibitorHandler.loadAll();
+
+    this.listenerHandler = new ListenerHandler(this, {
+      directory: path.join(__dirname, 'listeners'),
+    });
+    this.commandHandler.useListenerHandler(this.listenerHandler);
+    this.listenerHandler.loadAll();
   }
 }
 
